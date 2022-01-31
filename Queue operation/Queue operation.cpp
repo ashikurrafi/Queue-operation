f23289132e5d4d341e0f55a0a3ff3fed2786ff60
amiocse84@gmail.com <amiocse84@gmail.com>
2022-01-31 19:17:37 +0600

    bug fixed

--- Queue operation/Queue operation.cpp
+++ Queue operation/Queue operation.cpp
@@ -1,28 +1,91 @@
 #include<iostream>
+#define MAX 5
 using namespace std;
+void insert(void);
+void del();
+void display(void);
+int queue_array[MAX];
+int rear = -1;
+int front = -1;
 int main()
 {
-    int count, temp, j, array[30];
-    printf("How many numbers are u going to enter?: ");
-    scanf_s("%d", &count);
-    printf("Enter %d numbers: ", count);
-    for (int i = 0; i < count; i++)
-        scanf_s("%d", &array[i]);
-    /* This is the main logic of bubble sort algorithm */
-    for (int i = count - 2; i >= 0; i--)
+    int choice;
+    do
     {
-        for (j = 0; j <= i; j++)
+        cout << "1. Insert element to queue" << endl;
+        cout << "2. Delete element from queue" << endl;
+        cout << "3. Display all elements of queue" << endl;
+        cout << "4. Quite" << endl;
+        cout << "Enter your choice : ";
+        cin >> choice;
+        switch (choice)
         {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
+        case 1:
+            insert();
+            break;
+        case 2:
+            del();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+            break;
+        default:
+            printf("Wrong input");
+            break;
+        }
+    } while (choice != 4);
+    return 0;
+}
+
+void insert()
+{
+    int add_item;
+    if (rear == MAX - 1)
+    {
+        cout << "Queue Overflow" << endl;
+    }
+    else
+    {
+        if (front == -1)
+        {
+            front = 0;
+        }
+        cout << "Insert the element in queue : ";
+        cin >> add_item;
+        rear = rear + 1;
+        queue_array[rear] = add_item;
+    }
+}
+
+void del()
+{
+    if (front == -1 || front > rear)
+    {
+        cout << "Queue Underflow" << endl;
+    }
+    else
+    {
+        cout << "Element deleted from queue is : " << queue_array[front];
+        front = front + 1;
+    }
+}
+
+void display()
+{
+    int i;
+    if (front == -1)
+    {
+        cout << "Queue is empty" << endl;
+    }
+    else
+    {
+        cout << "Queue is :" << endl;
+        for (i = front; i <= rear; i++)
+        {
+            cout << queue_array[i] << endl;
         }
     }
-    printf("Sorted elements: ");
-    for (int i = 0; i < count; i++)
-        printf(" %d", array[i]);
-    return 0;
 }
